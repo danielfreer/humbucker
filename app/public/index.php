dd7476f5a52b0d53e6b742eb5a47c5ae132cdd6f
Daniel A. Freer <danielfreer@gmail.com>
2022-07-15 16:13:19 -0400

    refactor: move report to be index
    index.php just had debugging information in it, might as well move report to be index so that it's easier to find.

--- app/public/index.php
+++ app/public/index.php
@@ -1,10 +1,73 @@
 <?php
-    $pdo = new PDO('mysql:dbname=humbucker;host=sql', 'humbucker', 'password', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-    $query = $pdo->query('SHOW VARIABLES like "version"');
+require_once('../Repository.php');
+require_once('../CandyOrders.php');
+require_once('../CallOrders.php');
+require_once('../ReferralOrders.php');
+require_once('../SignatureOrders.php');
+require_once('../MiscOrders.php');
 
-    $row = $query->fetch();
+$repo = new Repository();
+$candy = new CandyOrders();
+$call = new CallOrders();
+$referral = new ReferralOrders();
+$signature = new SignatureOrders();
+$misc = new MiscOrders();
 
-    echo 'MySQL version:' . $row['Value'];
+$orders = $repo->orders();
+$candyOrders = $candy->filter($orders);
+$callOrders = $call->filter($orders);
+$referralOrders = $referral->filter($orders);
+$signatureOrders = $signature->filter($orders);
+$miscOrders = $misc->filter($candyOrders, $callOrders, $referralOrders, $signatureOrders, $orders)
 
-    phpinfo();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Report on Order Comments</title>
+    </head>
+    <body>
+        <h2>Comments about candy</h2>
+            <table>
+                <tbody>
+                    <?php forEach ($candyOrders as $order ): ?>
+                        <tr><td><?php echo htmlspecialchars($order->comments) ?></td></tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <h2>Comments about call me / don't call me</h2>
+            <table>
+                <tbody>
+                    <?php forEach ($callOrders as $order ): ?>
+                        <tr><td><?php echo htmlspecialchars($order->comments) ?></td></tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <h2>Comments about who referred me</h2>
+            <table>
+                <tbody>
+                    <?php forEach ($referralOrders as $order ): ?>
+                        <tr><td><?php echo htmlspecialchars($order->comments) ?></td></tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <h2>Comments about signature requirements upon delivery</h2>
+            <table>
+                <tbody>
+                    <?php forEach ($signatureOrders as $order ): ?>
+                        <tr><td><?php echo htmlspecialchars($order->comments) ?></td></tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <h2>Miscellaneous comments (everything else)</h2>
+            <table>
+                <tbody>
+                    <?php forEach ($miscOrders as $order ): ?>
+                        <tr><td><?php echo htmlspecialchars($order->comments) ?></td></tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+    </body>
+</html>
+
